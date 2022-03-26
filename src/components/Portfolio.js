adc4de18648f8a456f671dfadb8906f02e07e49c
Akhil anand <akhilanand.ak01@gmail.com>
2022-03-27 01:54:18 +0530

    minor changes

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -23,8 +23,8 @@ const Portfolio = () => {
     <Project img = {crownClothing} name= "Crown Clothing Mens"  gitHub="https://github.com/akhil-anand/crwn-clothing" hostingLink="https://crwn-clothing-mens.herokuapp.com/" title="This is a shopping website built using angular and material UI. It consists of shopping cart and guide throw till the end of checkout process" tags={["ReactJs", "Material UI", "shopping", ]}/>
     <Project img = {food} name= "Fortuna"  gitHub="" hostingLink="https://food1-e9a0a.web.app/" title="This is a static food website " tags={["ReactJs", "Material UI", "shopping", ]}/>
     <Project img = {memories} name= "Memories App"  gitHub="" hostingLink="https://memories-david.netlify.app/" title="A MERN stack application which uses mongo db for database and google auth for authentication" tags={["ReactJs", "Material UI", "memories", ]}/>
-    <Project img = {resumeBuilder} name= "Memories App"  gitHub="" hostingLink="https://resume-builder-79633.web.app/" title="A MERN stack application which uses mongo db for database and google auth for authentication" tags={["ReactJs", "Material UI", "memories", ]}/>
-    <Project img = {ticTacToe} name= "Memories App"  gitHub="" hostingLink="https://tiktactoe-48014.web.app/" title="Tic Tac Toe application" tags={["Angular", "Nebular"]}/>
+    <Project img = {resumeBuilder} name= "Resume Builder"  gitHub="" hostingLink="https://resume-builder-79633.web.app/" title="A MERN stack application which uses mongo db for database and google auth for authentication" tags={["ReactJs", "Material UI", "memories", ]}/>
+    <Project img = {ticTacToe} name= "TicTacToe App"  gitHub="" hostingLink="https://tiktactoe-48014.web.app/" title="Tic Tac Toe application" tags={["Angular", "Nebular"]}/>
     <Project img = {toDoList} name= "ToDo App"  gitHub="" hostingLink="https://todo-list-de42b.web.app/" title="ToDo list application" tags={["Angular"]}/>
     <Project img = {angularPortfolio} name= "Angular Portfolio"  gitHub="" hostingLink="https://port1-4eb3b.web.app/home" title="Angular Portfolio" tags={["Angular", "Material UI"]}/>
          </div>
